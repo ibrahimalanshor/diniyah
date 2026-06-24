c9be37471cb23c9154563ee80640bff0a69a31d2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-06-24 15:47:02 +0700

    linter

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+]);
